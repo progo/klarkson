1364d206f2058bed6cf35c5e94ce61b24e4a89dc
Mikael Puhakka <mike@fwwm.us>
2021-09-19 12:02:04 +0300

    Refactor album analysis bits into functions.

--- src/main/kotlin/Album.kt
+++ src/main/kotlin/Album.kt
@@ -70,11 +70,32 @@ fun collectIntoAlbums(mpd: Mpd, songs: Collection<MpdSong>) : List<Album> {
             Album(
                 artist = artist,
                 album = album,
-                year = songs.maxOfOrNull { it.year ?: 0 },
-                discCount = songs.maxOf { it.discNumber ?: 1 },
+                year = analyzeAlbumYear(songs),
+                discCount = analyzeDiscCount(songs),
                 songs = songs
             )
         }
 
     return albums
+}
+
+/**
+ * Take an album's [songs] and try to analyze a release year for it.
+ * The vast majority of songs will have same year tagged so no ambiguity there.
+ * Some select albums (VA collections, mostly) have a mixture of years across
+ * the songs and there we probably won't know the truth, but it has to be at
+ * least after the latest song on the album.
+ */
+fun analyzeAlbumYear(songs: Collection<Song>): Int? {
+    val maxYear = songs.maxOfOrNull { it.year ?: -1 }
+    return if (maxYear == -1) null else maxYear
+}
+
+
+/**
+ * Take an album's [songs] and try to deduce the disc/media count.
+ */
+fun analyzeDiscCount(songs: Collection<Song>) : Int {
+    // TODO add extra measures based on runtime perhaps?
+    return songs.maxOf { it.discNumber ?: 1 }
 }
