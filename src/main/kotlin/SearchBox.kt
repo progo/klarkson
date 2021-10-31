741c8d0a12997106d233ac3302e5a6ded57f2bae
Mikael Puhakka <mike@fwwm.us>
2021-10-31 15:51:09 +0200

    Highlight search box on focus

--- src/main/kotlin/SearchBox.kt
+++ src/main/kotlin/SearchBox.kt
@@ -2,16 +2,36 @@ package klarksonmainframe
 
 import java.awt.Color
 import java.awt.Font
+import java.awt.event.FocusAdapter
+import java.awt.event.FocusEvent
+import java.awt.event.FocusListener
 import javax.swing.JTextField
 
 class SearchBox : JTextField() {
+    private val backgroundFocus = Color.BLACK
+    private val backgroundDefocus = Color.GRAY
+
     init {
         foreground = Color.ORANGE
-        background = Color.BLACK
+        background = backgroundDefocus
+
         font = Font("sans serif", Font.PLAIN, 16)
         text = "Search..."
 
         SearchBoxActivator.initSearchBox(this)
+
+        addFocusListener(object : FocusAdapter() {
+            override fun focusGained(p0: FocusEvent?) {
+                background = backgroundFocus
+                repaint()
+            }
+
+            override fun focusLost(p0: FocusEvent?) {
+                background = backgroundDefocus
+                repaint()
+            }
+
+        })
     }
 
     fun focus() {
